880bbdea11de5493e1f578f94314da921ed743c9
Berke Tezergil <btezergil@atlassian.com>
2024-12-12 16:35:37 +0300

    Added nvim-tinygit

---
+++ lua/plugins/tinygit.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		"chrisgrieser/nvim-tinygit",
+		dependencies = "stevearc/dressing.nvim",
+		config = function()
+			local tinygit = require("tinygit")
+			vim.keymap.set("n", "gs", function()
+				tinygit.interactiveStaging()
+			end, { desc = "[g]it interactive [s]taging" })
+			vim.keymap.set("n", "ga", "<cmd>Gitsigns add_hunk<CR>", { desc = "[g]it [a]dd hunk" }) -- gitsigns.nvim
+			vim.keymap.set("n", "gc", function()
+				tinygit.smartCommit()
+			end)
+			vim.keymap.set("n", "gp", function()
+				tinygit.push()
+			end, { desc = "[g]it [p]ush" })
+		end,
+	},
+}
