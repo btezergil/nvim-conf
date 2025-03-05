9590a40152441ad4a56c2b7b4ff5ba76005d8e14
Berke Tezergil <btezergil@atlassian.com>
2025-03-05 23:37:30 +0300

    Extended noice.nvim

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -19,13 +19,39 @@ return {
 						["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 					},
 				},
-				-- you can enable a preset for easier configuration
-				presets = {
-					bottom_search = true, -- use a classic bottom cmdline for search
-					command_palette = true, -- position the cmdline and popupmenu together
-					long_message_to_split = true, -- long messages will be sent to a split
-					inc_rename = false, -- enables an input dialog for inc-rename.nvim
-					lsp_doc_border = false, -- add a border to hover docs and signature help
+				views = {
+					cmdline_popup = {
+						position = {
+							row = 18,
+							col = "50%",
+						},
+						size = {
+							width = 60,
+							height = "auto",
+						},
+					},
+					popupmenu = {
+						relative = "editor",
+						position = {
+							row = 21,
+							col = "50%",
+						},
+						size = {
+							width = 60,
+							height = 10,
+						},
+						border = {
+							style = "rounded",
+							padding = { 0, 1 },
+						},
+						win_options = {
+							winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+						},
+					},
+				},
+				routes = {
+					view = "notify",
+					filter = { event = "msg_showmode" },
 				},
 			})
 		end,
