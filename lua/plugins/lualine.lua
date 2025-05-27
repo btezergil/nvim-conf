67205034a6e6def608c58fbff6eba729d5729503
Berke Tezergil <btezergil@atlassian.com>
2025-05-27 19:02:39 +0300

    Lualine updates

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,47 +1,47 @@
 return {
-	{
-		"EdenEast/nightfox.nvim",
-	},
-	{
-		"linrongbin16/lsp-progress.nvim",
-		config = function()
-			require("lsp-progress").setup()
-		end,
-	},
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			vim.cmd("colorscheme nightfox")
-			require("lualine").setup({
-				extensions = { "lazy", "mason", "man", "trouble" },
-				sections = {
-					lualine_c = {
-						{
-							function()
-								return require("lsp-progress").progress()
-							end,
-						},
-						{
-							function()
-								local linters = require("lint").get_running()
-								if #linters == 0 then
-									return "󰦕"
-								end
-								return "󱉶 " .. table.concat(linters, ", ")
-							end,
-						},
-					},
-				},
-			})
+    {
+        "EdenEast/nightfox.nvim",
+    },
+    {
+        "linrongbin16/lsp-progress.nvim",
+        config = function()
+            require("lsp-progress").setup()
+        end,
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            vim.cmd("colorscheme nightfox")
+            require("lualine").setup({
+                extensions = { "lazy", "mason", "man", "trouble" },
+                sections = {
+                    lualine_c = {
+                        {
+                            function()
+                                return require("lsp-progress").progress()
+                            end,
+                        },
+                        {
+                            function()
+                                local linters = require("lint").get_running()
+                                if #linters == 0 then
+                                    return "󰦕"
+                                end
+                                return "󱉶 " .. table.concat(linters, ", ")
+                            end,
+                        },
+                    },
+                },
+            })
 
-			-- listen lsp-progress event and refresh lualine
-			vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
-			vim.api.nvim_create_autocmd("User", {
-				group = "lualine_augroup",
-				pattern = "LspProgressStatusUpdated",
-				callback = require("lualine").refresh,
-			})
-		end,
-	},
+            -- listen lsp-progress event and refresh lualine
+            vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
+            vim.api.nvim_create_autocmd("User", {
+                group = "lualine_augroup",
+                pattern = "LspProgressStatusUpdated",
+                callback = require("lualine").refresh,
+            })
+        end,
+    },
 }
